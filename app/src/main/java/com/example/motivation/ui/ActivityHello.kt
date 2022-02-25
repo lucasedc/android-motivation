3f442332ecb6419370be05f0484271f7d5f6082b
Lucas Carvalho <lucaseduardo@gmail.com>
2022-02-25 15:40:57 -0300

    Finalização do projeto

--- app/src/main/java/com/example/motivation/ui/ActivityHello.kt
+++ app/src/main/java/com/example/motivation/ui/ActivityHello.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.motivation.R
+import com.example.motivation.mock.Mock
 import com.example.motivation.utils.MotivationConstants
 import com.example.motivation.utils.SharedPreferencesUtils
 import kotlinx.android.synthetic.main.activity_hello.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ActivityHello : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSharedPreferences: SharedPreferencesUtils
+    private var mPhraseFilter: Int = MotivationConstants.INDEX_PHRASE.ALL_MESSAGE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +26,12 @@ class ActivityHello : AppCompatActivity(), View.OnClickListener {
         if (name != "") {
             tvName.text = name
         }
-
+        showAllMessage(MotivationConstants.PHRASE.ALL_MESSAGE)
         ivAll.setOnClickListener(this)
         ivHappy.setOnClickListener(this)
         ivSunny.setOnClickListener(this)
+
+        btnNewPhrase.setOnClickListener(this)
 
     }
 
@@ -36,6 +41,7 @@ class ActivityHello : AppCompatActivity(), View.OnClickListener {
             R.id.ivAll -> showAllMessage(MotivationConstants.PHRASE.ALL_MESSAGE)
             R.id.ivHappy -> showAllMessage(MotivationConstants.PHRASE.HAPPY_MESSAGE)
             R.id.ivSunny -> showAllMessage(MotivationConstants.PHRASE.SUNNY_MESSAGE)
+            R.id.btnNewPhrase -> showAllMessage(MotivationConstants.PHRASE.ALL_MESSAGE)
         }
 
 
@@ -47,12 +53,15 @@ class ActivityHello : AppCompatActivity(), View.OnClickListener {
         ivSunny.setColorFilter(resources.getColor(R.color.white))
         when (typeMessage) {
             MotivationConstants.PHRASE.ALL_MESSAGE -> {
+                tvPhrase.text = Mock().getPhrase(MotivationConstants.INDEX_PHRASE.ALL_MESSAGE)
                 ivAll.setColorFilter(resources.getColor(R.color.colorSelected))
             }
             MotivationConstants.PHRASE.HAPPY_MESSAGE -> {
+                tvPhrase.text = Mock().getPhrase(MotivationConstants.INDEX_PHRASE.HAPPY_MESSAGE)
                 ivHappy.setColorFilter(resources.getColor(R.color.colorSelected))
             }
             MotivationConstants.PHRASE.SUNNY_MESSAGE -> {
+                tvPhrase.text = Mock().getPhrase(MotivationConstants.INDEX_PHRASE.SUNNY_MESSAGE)
                 ivSunny.setColorFilter(resources.getColor(R.color.colorSelected))
             }
 
--- app/src/main/java/com/example/motivation/utils/MotivationConstants.kt
+++ app/src/main/java/com/example/motivation/utils/MotivationConstants.kt
@@ -10,4 +10,9 @@ abstract class MotivationConstants {
         const val ALL_MESSAGE = "all"
         const val SUNNY_MESSAGE = "sunny"
     }
+    object INDEX_PHRASE{
+        const val HAPPY_MESSAGE = 1
+        const val ALL_MESSAGE = 0
+        const val SUNNY_MESSAGE = 2
+    }
 }
